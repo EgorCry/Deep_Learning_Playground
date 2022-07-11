a74dff7bdca43fb6519b4b2ba0b005ce175b0f25
egorcry <abulapin@mail.ru>
2022-07-11 14:17:36 +0300

    Added Primes_in_Numbers version 2

--- Решение задач с собеседований/Primes_in_Numbers.py
+++ Решение задач с собеседований/Primes_in_Numbers.py
@@ -39,3 +39,38 @@ def prime_factors(number):
     if n != 1:
         nswr += f'({n})'
     return nswr
+
+
+def prime_factors_version_2(n):
+    i = 2
+    factors = {}
+    nswr = ''
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            if i not in factors:
+                factors[i] = 0
+            factors[i] += 1
+    if n > 1:
+        if n not in factors:
+            factors[n] = 0
+        factors[n] += 1
+    for k, v in factors.items():
+        if v == 1:
+            nswr += f'({k})'
+        else:
+            nswr += f'({k}**{v})'
+    return nswr
+
+
+import time
+
+
+test = 999_999_999_999_999_999_999_999_999_999
+
+start = time.time()
+print(prime_factors(test), f'Time: {round((time.time() - start) * 1000)}ms')
+start = time.time()
+print(prime_factors_version_2(test), f'Time: {round((time.time() - start) * 1000)}ms')
