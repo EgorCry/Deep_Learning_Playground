78c4467d4c1f9cfc2438d280d0d0a373cf2097eb
egorcry <abulapin@mail.ru>
2022-10-10 13:08:44 +0300

    Added Script for survey lung cancer recognition

--- Smoking_Cancer/Lung_Cancer_Recognition.py
+++ Smoking_Cancer/Lung_Cancer_Recognition.py
@@ -11,20 +11,23 @@ def model_predict():
     smoking = 1 if input('Please enter if you smoking (yes or no)\n').lower() == 'yes' else 0
     yellow_fingers = 1 if input('Please enter if you have yellow fingers (yes or no)\n').lower() == 'yes' else 0
     anxiety = 1 if input('Please enter if you have anxiety (yes or no)\n').lower() == 'yes' else 0
-    peer_pressure = 1 if input('Please enter if you have pressure from your peers (yes or no)\n').lower() == 'yes' else 0
+    peer_pressure = 1 if input('Please enter if you have pressure from your peers (yes or no)\n').lower() == 'yes' \
+        else 0
     chronic_disease = 1 if input('Please enter if you have any chronic disease (yes or no)\n').lower() == 'yes' else 0
     fatigue = 1 if input('Please enter if you have fatigue (yes or no)\n').lower() == 'yes' else 0
     allergy = 1 if input('Please enter if you have allergy (yes or no)\n').lower() == 'yes' else 0
     wheezing = 1 if input('Please enter if you have wheezing (yes or no)\n').lower() == 'yes' else 0
     alcohol_consuming = 1 if input('Please enter if you drinking alcohol (yes or no)\n').lower() == 'yes' else 0
     coughing = 1 if input('Please enter if you coughing (yes or no)\n').lower() == 'yes' else 0
-    shortness_of_breath = 1 if input('Please enter if you have difficulties with breathing (yes or no)\n').lower() == 'yes' else 0
-    swallowing_difficulty = 1 if input('Please enter if you swallowing with difficulties (yes or no)\n').lower() == 'yes' else 0
+    shortness_of_breath = 1 if input(
+        'Please enter if you have difficulties with breathing (yes or no)\n').lower() == 'yes' else 0
+    swallowing_difficulty = 1 if input(
+        'Please enter if you swallowing with difficulties (yes or no)\n').lower() == 'yes' else 0
     chest_pain = 1 if input('Please enter if you have chest pain (yes or no)\n').lower() == 'yes' else 0
 
     rqst = np.array([age, smoking, yellow_fingers, anxiety, peer_pressure,
-                      chronic_disease, fatigue, allergy, wheezing, alcohol_consuming,
-                      coughing, shortness_of_breath, swallowing_difficulty, chest_pain])
+                     chronic_disease, fatigue, allergy, wheezing, alcohol_consuming,
+                     coughing, shortness_of_breath, swallowing_difficulty, chest_pain])
 
     rqst_scaled = tf.reshape(rqst, [-1, 14])
 
@@ -37,8 +40,8 @@ def model_predict():
     if result_scaled == 1:
         print(f'You have lung cancer with {result}% sure.')
     else:
-        print(f"You don't have cancer with {100-result}% sure")
+        print(f"You don't have cancer with {100 - result}% sure")
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     model_predict()
