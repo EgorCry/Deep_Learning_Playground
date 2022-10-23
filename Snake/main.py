0090d1dcfdc56dd29942ea74e012fe841f7b6e78
egorcry <abulapin@mail.ru>
2022-10-24 00:19:32 +0300

    MinUpd Snake Game

--- Snake/main.py
+++ Snake/main.py
@@ -5,6 +5,10 @@ pygame.init()
 dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
 pygame.display.set_caption('Snake')
+
+blue = (0, 0, 255)
+red = (255, 0, 0)
+
 game_over = False
 
 if __name__ == '__main__':
@@ -13,6 +17,8 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 game_over = True
             print(event)
+        pygame.draw.rect(dis, blue, [200, 150, 10, 10])
+        pygame.display.update()
 
     pygame.quit()
     quit()
