1985bc78639771b24f8e657ab95fb40059059c3e
egorcry <abulapin@mail.ru>
2022-11-01 00:31:31 +0300

    Finished Task for Sber Internship with Linear Regression

---
+++ Datasets/Sber/main.py
@@ -0,0 +1,78 @@
+from model import Model
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+
+def change_initial_dataframe(dataframe):
+    dataframe.columns = dataframe.columns.str.lower()
+    dataframe['reportdate'] = dataframe['reportdate'].str.replace('.', '/')
+    dataframe["reportdate"] = pd.to_datetime(dataframe["reportdate"], infer_datetime_format=True)
+
+    return dataframe
+
+
+def create_dataset(data, window):
+    x = []
+    y = []
+    for i in range(window, data.shape[0]):
+        x.append(data[i - window:i, 0])
+        y.append(data[i, 0])
+    x = np.array(x)
+    y = np.array(y)
+    return x, y
+
+
+def create_dataframe(dates, values):
+    nswr = pd.DataFrame({'dates': dates, 'values': values})
+    return nswr
+
+
+def show_plot(forecast, init_data):
+    plt.figure(figsize=(14, 7))
+
+    plt.plot(forecast['dates'], forecast['values'])
+    plt.plot(init_data['reportdate'], init_data['value'])
+
+    plt.show()
+
+
+def model_forecast(init_date, window, data):
+    window = int(window[:-1]) * 30
+
+    data = data['value'].values.reshape((-1, 1))
+
+    x_train, y_train = create_dataset(data, window)
+
+    model = Model(window)
+    model.fit(x_train, y_train)
+
+    prediction = model.predict(data)
+
+    dates = model.make_dates(init_date)
+
+    forecast = create_dataframe(dates, prediction)
+
+    print(forecast)
+
+    return model, forecast
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('DS_Sber.csv', sep=';')
+    df = change_initial_dataframe(df)
+    data = df.copy()
+    
+    init_date = input('Input initial date in format: YYYY-MM-DD\n')
+    window = input('Input how many months do you need: 1M - 12M\n')
+
+    init_date = np.datetime64(init_date)
+    ind = data[data['reportdate'] == init_date].index
+    data = data[:ind[0]]
+    df = df[:ind[0]]
+
+    model, forecast = model_forecast(init_date, window, data)
+
+    show_plot(forecast, df)
---
+++ Datasets/Sber/model.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+class Model:
+    def __init__(self, window):
+        self.model = LinearRegression()
+        self.window = window
+
+    def fit(self, x, y):
+        self.model.fit(x, y)
+
+    def score_model(self, x, y):
+        return self.model.score(x, y)
+
+    def intercept_model(self):
+        return self.model.intercept_
+
+    def coef_model(self):
+        return self.model.coef_
+
+    def predict(self, data):
+        nswr = data.copy()
+
+        for _ in range(self.window):
+            x_data = nswr[-self.window:].reshape(1, self.window)
+
+            prediction = self.model.predict(x_data)
+            
+            nswr = np.append(nswr, prediction)
+
+        return nswr[-self.window:]
+
+    def make_dates(self, init_date):
+        date = init_date
+
+        dates = np.array([]).astype('datetime64')
+
+        for _ in range(self.window):
+            date += np.timedelta64(1, 'D')
+
+            dates = np.append(dates, date)
+
+        return dates
